24580ec5c9873812685200a658899ac7a691faa4
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2020-09-01 17:35:59 +0800

    修改页面

--- src/views/payAmount.vue
+++ src/views/payAmount.vue
@@ -7,30 +7,36 @@
         为了确保您的资金安全，请设置支付密码为了确保您的资金安全，请设置支付密码为了确保您的资金安全，请设置支付密码
       </md-notice-bar>
     </div>
-    <img class="qm-logo" :src="logo2" />
+    <img class="qm-logo" :src="logo" />
     <div class="md-example-child md-example-child-input-item-3 section">
       <md-field title="支付金额(元)">
         <md-input-item
+          v-model="value"
           class="qm-input-large-text"
           type="money"
-          v-model="value"
           placeholder="0.00"
-          size="large"
+          :size="size"
           is-amount
+          maxlength="9"
+          is-formative
           align="center"
           @change="moneyFormate"
+          @confirm="confirmMoney"
           is-virtual-keyboard
           precision="2"
+          clearable
+          brief="支付金额不能超过10万"
         >
         </md-input-item>
-        <span class="qm-text-size-primary">支付金额不能超过10万</span>
       </md-field>
     </div>
     <md-field class="section" title="备注">
-      <md-input-item
+      <md-textarea-item
+        ref="demo0"
         title="备注一："
+        autosize
         placeholder="请输入备注一"
-      ></md-input-item>
+      />
       <md-input-item
         title="备注二："
         placeholder="请输入备注二"
@@ -40,8 +46,8 @@
         placeholder="请输入备注三"
       ></md-input-item>
     </md-field>
-    <md-action-bar :actions="actionBarData"> </md-action-bar>
 
+    <md-action-bar :actions="actionBarData"> </md-action-bar>
     <md-dialog
       title="确认金额"
       :closable="true"
@@ -50,11 +56,7 @@
     >
       确定提交支付￥{{ value }}元吗，点击确认则跳转第三方支付系统
     </md-dialog>
-    <div
-      class="md-example-child md-example-child-popup md-example-child-popup-0"
-    >
-      <!-- <md-button @click="showPopUp('center')">屏幕中弹出</md-button> -->
-
+    <div class="md-example-child">
       <md-popup v-model="isPopupShow.center">
         <div class="md-example-popup md-example-popup-center">
           <div
@@ -77,16 +79,13 @@
     </div>
   </div>
 </template>
-
 <script>
 import {
   Dialog,
   FieldItem,
-  Button,
   Field,
   InputItem,
   CheckBox,
-  Agree,
   ActionBar,
   Tag,
   Icon,
@@ -94,22 +93,16 @@ import {
   ActivityIndicator,
   Popup,
   PopupTitleBar,
+  TextareaItem,
 } from "mand-mobile";
-import logo1 from "@/assets/images/logo.png";
-import logo2 from "@/assets/images/logo-vertial.png";
-import logo3 from "@/assets/images/logoIcon.png";
+import logo from "@/assets/images/logo-vertial.png";
 export default {
   name: "app",
   data() {
     return {
-      type: "month",
-      value: 0,
-      isAgree: false,
-      open: false,
-      logo1,
+      value: "",
       isPopupShow: {},
-      logo2,
-      logo3,
+      logo,
       actionBarData: [
         {
           text: "确认支付",
@@ -129,42 +122,39 @@ export default {
           },
         ],
       },
-      agreeConf: {
-        checked: true,
-        name: "agree0",
-        size: "md",
-        disabled: false,
-        introduction: "选中状态",
-      },
     };
   },
   computed: {
     size() {
-      return this.value.length > 10 ? "small" : "large";
+      return this.value.length > 9 ? "small" : "large";
     },
   },
   components: {
-    [Button.name]: Button,
     [FieldItem.name]: FieldItem,
     [Field.name]: Field,
     [InputItem.name]: InputItem,
     [CheckBox.name]: CheckBox,
     [ActionBar.name]: ActionBar,
     [Tag.name]: Tag,
-    [Agree.name]: Agree,
     [Icon.name]: Icon,
     [NoticeBar.name]: NoticeBar,
     [ActivityIndicator.name]: ActivityIndicator,
     [Popup.name]: Popup,
     [PopupTitleBar.name]: PopupTitleBar,
     [Dialog.name]: Dialog,
+    [TextareaItem.name]: TextareaItem,
   },
   methods: {
     showBasicDialog() {
       this.basicDialog.open = true;
     },
+    confirmMoney(name, value) {
+      console.log(name, value, "%%%%");
+    },
     moneyFormate(name, value) {
       this.value = value;
+      // if (this.value === 88) {
+      // }
       console.log(name, value);
     },
     showPopUp(type) {
@@ -185,7 +175,9 @@ export default {
 body {
   background: #f8f8f8;
 }
-
+#app {
+  padding-bottom: 0.6rem;
+}
 .section {
   margin-bottom: 20px;
   background: #fff;
@@ -203,15 +195,6 @@ body {
 
 .qm-input-large-text.md-input-item .md-input-item-control .md-input-item-fake {
   font-size: 0.48rem !important;
-}
-
-.qm-text-size-primary {
-  font-size: 0.16rem;
-  color: #999999;
-  text-align: left;
-  display: block;
-  flex-direction: column;
-  padding: 0 0.32rem;
 }
 
 .md-example-child-md-activity-indicator-1 {
@@ -245,6 +228,10 @@ body {
 }
 
 .md-popup-mask {
-  background-color: transparent !important;
+  // background-color: transparent !important;
+}
+
+textarea {
+  padding-top: 0 !important;
 }
 </style>
