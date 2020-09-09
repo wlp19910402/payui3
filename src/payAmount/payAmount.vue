2991a1a1c11c84d6e253c4012be06b0a9e36ef34
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2020-09-09 16:36:31 +0800

    修改bug

--- src/payAmount/payAmount.vue
+++ src/payAmount/payAmount.vue
@@ -40,10 +40,11 @@
           :placeholder="item.placeholder"
           :id="item.id"
           :name="item.name"
+          :value="item.value"
           left
         ></md-input-item>
         <div v-if="item.type === 'select'">
-          <qm-selector :selectorData="item"></qm-selector>
+          <qm-selector :ref="item.name" :selectorData="item"></qm-selector>
         </div>
       </div>
     </md-field>
@@ -58,7 +59,7 @@
       确定提交支付￥{{ moneyVal }}元吗，点击确认则跳转第三方支付系统
     </md-dialog>
     <div class="md-example-child">
-      <md-popup v-model="isPopupShow.center">
+      <md-popup :mask-closable="false" v-model="isloadingShow">
         <div class="md-example-popup md-example-popup-center">
           <div
             class="md-example-child md-example-child-md-activity-indicator md-example-child-md-activity-indicator-1"
@@ -104,7 +105,13 @@ export default {
   data() {
     return {
       moneyVal: "",
-      isPopupShow: {},
+      //初始化备注表单的内容
+      remarkList: {
+        remark1: null,
+        remark2: null,
+        remark3: null,
+      },
+      isloadingShow: false,
       logo,
       formData: [],
       actionBarData: [
@@ -130,8 +137,8 @@ export default {
     };
   },
   async created() {
+    //获取static中的form表单的数据，备注一，备注二，备注三
     await axios.get("/static/formNote.json").then((res) => {
-      console.log(res.data);
       let result = res.data;
       if (result.status === "0") {
         this.formData = result.formList;
@@ -141,9 +148,11 @@ export default {
     });
   },
   computed: {
+    //当输入的金额数字长度大于9时，将字体大小切换成小的
     size() {
       return this.moneyVal.length > 9 ? "small" : "large";
     },
+    //当输入金额的值大于10，则提示信息
     moneyErrorInfo() {
       return parseFloat(this.moneyVal) > 1000000000
         ? "支付金额不能超过10亿"
@@ -166,65 +175,95 @@ export default {
     QmSelector,
   },
   methods: {
+    //显示确认支付的弹出框
     showBasicDialog() {
       this.basicDialog.open = true;
     },
     confirmMoney(name, value) {
       console.log(name, value);
     },
+    //输入金额的格式进行转换成标准的格式化。xxxxxxx.xx 小数点只支持到2位
     moneyFormate(name, value) {
       let money = value;
+      //如果只输入了.,就默认指向0.
       if (value === ".") {
         money = "0.";
       }
-      money = parseFloat(money).toString();
+      //控制金额小数点为两位
       let arrTmp = money.split(".");
       if (arrTmp.length === 2) {
         if (arrTmp[1].length > 2) {
           arrTmp[1] = arrTmp[1].slice(0, 2);
           money = arrTmp[0] + "." + arrTmp[1];
         }
+      } else {
+        //控制金额 000889格式，转换成标准的889
+        money = parseFloat(money).toString();
       }
+      //当dom变化后进行重新给金额赋值，反正没定义上
       this.$nextTick(() => {
         this.moneyVal = money;
-        if (parseFloat(this.moneyVal) > 0&& parseFloat(this.moneyVal)<1000000000) {
+        if (
+          parseFloat(this.moneyVal) > 0 &&
+          parseFloat(this.moneyVal) < 1000000000
+        ) {
           this.actionBarData[0].disabled = false;
         } else {
           this.actionBarData[0].disabled = true;
         }
       });
     },
-    showPopUp(type) {
-      this.$set(this.isPopupShow, type, true);
-    },
-    hidePopUp(type) {
-      this.$set(this.isPopupShow, type, false);
-    },
+    //确认提交金额去支付页面
     onBasicConfirm() {
-      this.showPopUp("center");
+      //关闭确认去支付的对话框
+      this.onBasicCancel();
+      //调用loading
+      this.isloadingShow = true;
+      //根据填写和设置的值给备注表单进行赋值
+      if (this.formData.length > 0) {
+        this.formData.forEach((item, index) => {
+          let obj = document.getElementsByName(item.name)[0];
+          if (item.type === "select") {
+            this.remarkList["remark" + (index + 1)] = this.$refs[
+              item.name
+            ][0].selectorValue;
+          } else if (item.type === "input") {
+            this.remarkList["remark" + (index + 1)] = obj.value;
+          }
+        });
+      }
+      //调用接口
+      this.fetchRequestPayAmount();
+    },
+    //提交支付金额备注等信息去支付页面
+    async fetchRequestPayAmount() {
       axios
         .post("/scenefront/tran/paymentquery", {
           merId: this.$route.query.merId,
           termId: this.$route.query.termId,
           tranAmt: this.moneyVal,
+          ...this.remarkList,
         })
         .then((res) => {
-          this.hidePopUp("center");
+          //关闭loading
+          this.isloadingShow = false;
           let responseResult = res.data;
           if (responseResult.code === "success") {
-            Toast.info("请求成功，正在跳转支付页面");
+            // Toast.info("请求成功，正在跳转支付页面");
+            Toast.info(responseResult.msg);
             setTimeout(() => {
-              window.location.replace(responseResult.qrCode);
+              window.location = responseResult.qrCode;
             }, 1000);
           } else {
             Toast.info("请求失败，请重试");
           }
         })
         .catch((err) => {
+          //关闭loading
+          this.isloadingShow = false;
           Toast.info("请求支付失败");
           console.log(err);
         });
-      this.basicDialog.open = false;
     },
     onBasicCancel() {
       this.basicDialog.open = false;
@@ -233,9 +272,9 @@ export default {
 };
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scope>
 body {
-  background: #f8f8f8;
+  background: #f8f8f8 !important;
 }
 
 .qm-action-bar-bg {
@@ -256,10 +295,9 @@ body {
 }
 
 .qm-logo {
-  margin: 0.2rem auto;
+  margin: 0.2rem 0;
   width: 2.4rem;
   height: auto;
-  display: block;
 }
 
 .qm-input-large-text.md-input-item .md-input-item-control .md-input-item-fake {
--- src/waitPayResult/waitPayResult.vue
+++ src/waitPayResult/waitPayResult.vue
@@ -81,7 +81,10 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+body {
+  background: #ffffff;
+}
 .qm-model-center {
   width: 100%;
   position: absolute;
